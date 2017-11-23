4e94fc470e793788ee4bcb3c64de8e96ea91f7cf
Gordon Wang <gwang@wirelessprv-10-192-196-224.near.illinois.edu>
2017-11-23 14:46:40 -0600

    Add basic CUDA implementation.

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -8,76 +8,74 @@ namespace mxnet
 {
 namespace op
 {
-
+#define TILE_SIZE 32
 
 template<typename gpu, typename DType>
-__global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int B, const int M, const int C, const int H, const int W, const int K) {
+__global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int M, const int C, const int H, const int W, const int K, const int W_grid)
+{
+	const int H_out = H - K + 1;
+	const int W_out = W - K + 1;
 
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
+	#define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
+	#define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
+	#define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
-    #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
-    #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
-    #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
+	int b = blockIdx.x;
+	int m = blockIdx.y;
+	int h = blockIdx.z / W_grid + threadIdx.y;
+	int w = blockIdx.z % W_grid + threadIdx.x;
+	DType acc = 0;
 
-    /*
-        Your code here!
-    */
+	if(h < H_out && w < W_out)
+	{
+		for(int c = 0; c < C; c++)
+		{
+			for(int p = 0; p < K; p++)
+			{
+				for(int q = 0; q < K; q++)
+				{
+					acc += x4d(b, c, h+p, w+q) * k4d(m, c, p, q);    				
+				}
+			}
+		}
+		y4d(b, m, h, w) = acc;
+	}
 
-    #undef y4d
-    #undef x4d
-    #undef k4d
+	#undef y4d
+	#undef x4d
+	#undef k4d
 }
-
-
-
 
 // This function is called by new-inl.h
 // Any code you write should be executed by this function
 template<typename gpu, typename DType>
-void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w) {
-    
+void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w)
+{
+	// You'll probably need to launch kernels against the right stream to keep MXNet happy
+	cudaStream_t s = y.stream_->stream_;
 
-    // Use mxnet's CHECK_EQ to do assertions.
-    // CHECK_EQ(0, 1) << "Missing an ECE408 GPU implementation!";
+	// Extract the tensor dimensions into B,M,C,H,W,K
+	const int B = x.shape_[0];
+	const int M = y.shape_[1];
+	const int C = x.shape_[1];
+	const int H = x.shape_[2];
+	const int W = x.shape_[3];
+	const int K = w.shape_[3];
 
-    // You'll probably need to launch kernels against the right stream to keep MXNet happy
-    // cudaStream_t s = y.stream_->stream_;
-    cudaStream_t s = y.stream_->stream_;
+	const int W_grid = (int)ceil((W - K + 1) / ((float)TILE_SIZE));
+	const int H_grid = (int)ceil((H - K + 1) / ((float)TILE_SIZE));
+	const int Z = W_grid * H_grid;
 
-    // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
-    const int B = x.shape_[0];
-    const int M = y.shape_[1];
-    const int C = x.shape_[1];
-    const int H = x.shape_[2];
-    const int W = x.shape_[3];
-    const int K = w.shape_[3];
-    
 	// Set the kernel dimensions
-    dim3 gridDim(0);
-    dim3 blockDim(0);
+	dim3 blockDim(TILE_SIZE, TILE_SIZE, 1);
+	dim3 gridDim(B, M, Z);
 
-    // Call the kernel
-    // forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
-    forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
-
-    // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
-    MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-
+	// Call the kernel
+	forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, M, C, H, W, K, W_grid);
+	MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 }
 
-
-
+#undef TILE_SIZE
 }
 }
-
 #endif
