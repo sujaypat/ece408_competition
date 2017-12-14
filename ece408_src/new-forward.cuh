ac4bbcb5297274fd77e6c57b8ffc18faaf6b5959
Tanishq Dubey <tanishq.dubey@gmail.com>
2017-12-14 01:16:12 -0600

    in or out no douubt

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -4,139 +4,113 @@
 
 #include <mxnet/base.h>
 
+#define TILE_WIDTH 28
+#define PIC_PER_BLOCK 10
+
 namespace mxnet
 {
 namespace op
 {
-	#define TILE_SIZE 4
-    #define TILE_WIDTH 4
 
-	template<typename gpu, typename DType>
-	__global__ void matrixMultiply(DType *A, DType const *B, DType const *C) {
-		int numARows = 50;
-		int numAColumns = 25;
-		int numBRows = 25;
-		int numBColumns = 576;
-		int numCRows = 50;
-		int numCColumns = 576;
+__constant__ float constant_kernel[1250];
 
-		unsigned row, col;
-		__shared__ float tileA[TILE_SIZE][TILE_SIZE];
-		__shared__ float tileB[TILE_SIZE][TILE_SIZE];
-		float sum = 0.0;
-	
-		row = blockIdx.y*blockDim.y + threadIdx.y;
-		col = blockIdx.x*blockDim.x + threadIdx.x;
+template<typename gpu, typename DType>
+__global__ void forward_kernel(DType *y, const DType *x, const DType *k, const int B, const int M, const int C, const int H, const int W, const int K) {
 
-		for(int m = 0; m < (TILE_SIZE+numAColumns-1)/TILE_SIZE+1; m++) {
-			if(row < numARows && m*TILE_SIZE+threadIdx.x < numAColumns) {
-				tileA[threadIdx.y][threadIdx.x] = A[row*numAColumns + m*TILE_SIZE+threadIdx.x];
-			} else {
-				tileA[threadIdx.y][threadIdx.x] = 0.0;
-			}
+    /*
+    Modify this function to implement the forward pass described in Chapter 16.
+    We have added an additional dimension to the tensors to support an entire mini-batch
+    The goal here is to be correct AND fast.
+    We have some nice defs for you below to simplify indexing. Feel free to use them, or create your own.
+    */
+    extern __shared__ __align__(sizeof(DType)) unsigned char my_smem[];
+    DType *shmem = reinterpret_cast<DType *>(my_smem);
+    DType* x_shared = &shmem[0];
 
-			if(m*TILE_SIZE+threadIdx.y < numBRows && col < numBColumns) {
-				tileB[threadIdx.y][threadIdx.x] = B[(m*TILE_SIZE+threadIdx.y)*numBColumns + col];
-			} else {
-				tileB[threadIdx.y][threadIdx.x] = 0.0;
-			}
-			__syncthreads();
+    int block_num = blockIdx.x;
+    int local_h = threadIdx.y;
+    int local_w = threadIdx.x;
+    int input_size = 28 * 28;
+    int output_size = 24 * 24;
+    int read_base = block_num * PIC_PER_BLOCK * 28 * 28 + local_h * 28 + local_w;
+    int write_base = 0;
+    DType sum = 0.0;
+    // load 28 * 28 input
+    for (int index = 0; index < PIC_PER_BLOCK; index++) {
+      x_shared[index * input_size + local_h * 28 + local_w] = x[read_base];
+      read_base += input_size; // each block load adjacent 10 images
+    }
+    __syncthreads();
 
-			if(row < numCRows && col < numCColumns) {
-					for(int k = 0; k < TILE_SIZE; k++) {
-						sum += tileA[threadIdx.y][k] * tileB[k][threadIdx.x];
-					}
-			}
-			__syncthreads();
-		}
+    // compute
 
-		if(row < numCRows && col < numCColumns) {
-				C[row*numCColumns+col] = sum;
-		}
-	}
+    if (local_h < 24 && local_w < 24) {
+      write_base = block_num * PIC_PER_BLOCK * 50 * output_size + local_h * 24 + local_w;
+      for (int index = 0; index < PIC_PER_BLOCK; index++) {
+        #pragma unroll 15
+        for (int kernel_index = 0; kernel_index < 50; kernel_index++) {
+          sum = 0.0;
+          for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                sum += x_shared[index * input_size + (local_h + i) * 28 + local_w + j] *
+                  constant_kernel[kernel_index * 25 + i * 5 + j];
+              }
+          }
 
-	template<typename gpu, typename DType>
-	__global__ void unroll_Kernel(int C, int H, int W, int K, DType* X, DType* X_unroll)
-	{
-		int c, s, h_out, w_out, h_unroll, w_base, p, q;
-		int t = blockIdx.x * 2048 + threadIdx.x;
-		int h_out = H - K + 1;
-		int w_out = W - K + 1;
-		int w_unroll = H_out * W_out;
-		if (t < C * w_unroll) {}
-		c = t / w_unroll;
-		s = t % w_unroll;
-		h_out = s / w_out;
-		w_out = s % w_out;
-		h_unroll = h_out * w_out + w_out;
-		w_base = c * K * K;
-		for(p = 0; p < K; p++) {
-			for(q = 0; q < K; q++) {
-				w_unroll = w_base + p * K + q;
-				X_unroll[w_unroll, h_unroll] = X[c, h_out + p, w_out + q];
-			}
-		}
-	}
+          // write_base = (block_num) * PIC_PER_BLOCK * 50 * output_size +
+          //   index * (B / PIC_PER_BLOCK) * 50 * output_size
+          //   + kernel_index * output_size + local_h * 24 + local_w;
+          y[write_base + kernel_index * output_size] = sum; // this should be kernel related
+        }
+        write_base += 50 * output_size;
+      }
+    }
+}
+
+
+
 
 // This function is called by new-inl.h
 // Any code you write should be executed by this function
-	template<typename gpu, typename DType>
-	void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w)
-	{
-		// You'll probably need to launch kernels against the right stream to keep MXNet happy
-		//cudaStream_t s = y.stream_->stream_;
+template<typename gpu, typename DType>
+void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w) {
 
-		// Extract the tensor dimensions into B,M,C,H,W,K
-		/**
-		 * C = number of input feature maps
-		 * H = height of each input map
-		 * W = width of each input map
-		 * M = number of output feature maps
-		 * K = Height and width of each filter bank
-		 **/
-		const int B = x.shape_[0];
-		const int M = y.shape_[1];
-		const int C = x.shape_[1];
-		const int H = x.shape_[2];
-		const int W = x.shape_[3];
-		const int K = w.shape_[3];
 
-		const int W_grid = (int)ceil((W - K + 1) / ((float)TILE_SIZE));
-		const int H_grid = (int)ceil((H - K + 1) / ((float)TILE_SIZE));
-		//const int Z = W_grid * H_grid;
+    // Use mxnet's CHECK_EQ to do assertions.
+    // CHECK_EQ(0, 1) << "Starting a GPU implementation based on share memory!";
 
-		int H_out = H - K + 1;
-		int W_out = W - K + 1;
-		//int num_threads = C * H_out * W_out;
-		int num_blocks = ceil((C * H_out * W_out) / 2048);
+    // You'll probably need to launch kernels against the right stream to keep MXNet happy
+    cudaStream_t s = y.stream_->stream_;
 
-		DType *x_unrolled;
-		DType *W_unrolled;
-		cudaMalloc(&x_unrolled, B * K * K * H_out * W_out * sizeof(DType));
-		cudaMalloc(&W_unrolled, M * K * K * sizeof(DType));
+    // Extract the tensor dimensions into B,M,C,H,W,K
+    const int B = x.shape_[0]; // input batch
+    const int M = w.shape_[0]; // output channel number
+    const int C = x.shape_[1]; // input channel number
+    const int H = x.shape_[2]; // input height
+    const int W = x.shape_[3]; // input width
+    const int K = w.shape_[2]; // kernel size
 
-		unroll_Kernel<<<num_blocks, 2048>>>(C, H, W, K , x.dptr_, x_unrolled);
+    // Set the kernel dimensions
+    // int Hout = H - K + 1;
+    // int Wout = W - K + 1;
+    // int W_numOfTiles = (Wout - 1) / TILE_WIDTH + 1;
+    // int H_numOfTiles = (Hout - 1) / TILE_WIDTH + 1;
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 gridDim((B / PIC_PER_BLOCK), 1, 1);
 
-		cudaDeviceSynchronize();
+    // allocate constant_kernel
+    cudaMemcpyToSymbol(constant_kernel, w.dptr_, sizeof(float) * 50 * 25, 0, cudaMemcpyDeviceToDevice);
+    // Call the kernel                                0 is sharemem s is stream
+    forward_kernel<gpu, DType><<<gridDim, blockDim, PIC_PER_BLOCK * sizeof(DType) * H * W, s>>>(y.dptr_,x.dptr_,w.dptr_, B, M, C, H, W, K);
 
-		dim3 blockDim(ceil((576/(double)TILE_WIDTH)), ceil(50/(double)TILE_WIDTH), 1);
-		dim3 gridDim(TILE_SIZE, TILE_SIZE, 1);
+    // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
+    MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
-		matrixMultiply<<<gridDim, blockDim>>>(x_unrolled, w.dptr_, y.dptr_);
+}
 
-		// // Set the kernel dimensions
-		// dim3 blockDim(TILE_SIZE, TILE_SIZE, 1);
-		// dim3 gridDim(B, M, Z);
-		
-		// dim3 unroll_blockDim(TILE_SIZE, TILE_SIZE, 1);
-		// dim3 unroll_gridDim(B, C, Z);
 
-		// // Call the kernel
-		// forward_kernel<gpu, DType><<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, M, C, H, W, K, W_grid);
-		MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-	}
 
-#undef TILE_SIZE
 }
 }
+
 #endif
